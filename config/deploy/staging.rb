56294b4ef2a4252d0091071b8ab4c1a791712137
Paulo Barcelos <paulo@azucrina.org>
2019-11-14 18:01:07 +0100

    configured capitrano wp cli

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,13 @@ set :stage, :staging
 
 set :branch, ENV.fetch('REVISION', 'develop')
 
+# WP CLI
+# ======
+set :wpcli_remote_url, 'https://learning-cms-stage.strawbees.com'
+set :wpcli_local_url, 'http://learning-cms.lndo.site'
+set :wpcli_backup_db, true
+
+
 # Simple Role Syntax
 # ==================
 #role :app, %w{deploy@example.com}
