599ce8b21c1d1f3d2b46f6f97e7e43c12929535e
Paulo Barcelos <paulo@azucrina.org>
2019-11-21 22:58:02 +0100

    added palette

--- web/app/themes/learning-cms-theme/inc/add-theme-support.php
+++ web/app/themes/learning-cms-theme/inc/add-theme-support.php
@@ -33,23 +33,52 @@ add_theme_support( 'editor-font-sizes', array(
 		'size' => 48,
 		'slug' => 'huge'
 	)
-) );
-add_theme_support(
-	'editor-color-palette', array(
-		array(
-			'name'  => esc_html__( 'Red', 'strawbees-headless-wp' ),
-			'slug' => 'red',
-			'color' => 'red',
-		),
-		array(
-			'name'  => esc_html__( 'Green', 'strawbees-headless-wp' ),
-			'slug' => 'green',
-			'color' => 'green',
-		),
-		array(
-			'name'  => esc_html__( 'Yellow', 'strawbees-headless-wp' ),
-			'slug' => 'yellow',
-			'color' => 'rgb(255,255,0)',
-		)
-	)
-);
+));
+add_theme_support( 'editor-color-palette', array(
+	array(
+		'name'  => esc_html__( 'Transparent', 'strawbees-headless-wp' ),
+		'slug' => 'transparent',
+		'color' => 'rgba(0,0,0,0)',
+	),
+	array(
+		'name'  => esc_html__( 'White', 'strawbees-headless-wp' ),
+		'slug' => 'white',
+		'color' => '#FFFFFF',
+	),
+	array(
+		'name'  => esc_html__( 'Light Grey', 'strawbees-headless-wp' ),
+		'slug' => 'lightGrey',
+		'color' => '#EBEBEB',
+	),
+	array(
+		'name'  => esc_html__( 'Dark Grey', 'strawbees-headless-wp' ),
+		'slug' => 'darkGrey',
+		'color' => '#525252',
+	),
+	array(
+		'name'  => esc_html__( 'Black', 'strawbees-headless-wp' ),
+		'slug' => 'black',
+		'color' => '#303030',
+	),
+	array(
+		'name'  => esc_html__( 'Blue', 'strawbees-headless-wp' ),
+		'slug' => 'blue',
+		'color' => '#00ADEE',
+	),
+	array(
+		'name'  => esc_html__( 'Green', 'strawbees-headless-wp' ),
+		'slug' => 'green',
+		'color' => '#00A99D',
+	),
+	array(
+		'name'  => esc_html__( 'Pink', 'strawbees-headless-wp' ),
+		'slug' => 'pink',
+		'color' => '#FF7381',
+	),
+	array(
+		'name'  => esc_html__( 'Yellow', 'strawbees-headless-wp' ),
+		'slug' => 'yellow',
+		'color' => '#FEB702',
+	),
+));
+add_theme_support( '__experimental-editor-gradient-presets', array() );
