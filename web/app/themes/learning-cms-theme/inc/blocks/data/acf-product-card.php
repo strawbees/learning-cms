1274bd58825782929b7eaf6a726b5acd1c229f15
Paulo Barcelos <paulo@azucrina.org>
2019-12-03 17:40:25 +0100

    button & card

--- web/app/themes/learning-cms-theme/inc/blocks/data/acf-product-card.php
+++ web/app/themes/learning-cms-theme/inc/blocks/data/acf-product-card.php
@@ -1,17 +1,19 @@
 <?php
-function get_block_data_acf_product_card( $block ) {
+function get_block_data_acf_product_card( $block, $post_id ) {
 	$data = resolve_attrs_prop($block['attrs'], 'data', array());
 	$image = convert_to_cdn_url(
 		$data['image'] ? wp_get_attachment_image_src(
 			$data['image'], 'large'
 		)[0] : ''
 	);
+	$color = get_field( 'primary_color', $post_id );
 	return array(
-		'title'        => $data['title'],
-		'image'        => $image,
-		'description'  => $data['description'],
-		'buyButtonURL' => convert_to_relative_url( $data['buy_button_url'] ),
-		'buyButtonCTA' => $data['buy_button_cta'],
+		'title'          => $data['title'],
+		'color'          => $color,
+		'imageUrl'       => $image,
+		'excerpt'        => $data['description'],
+		'buyButtonLink'  => convert_to_relative_url( $data['buy_button_url'] ),
+		'buyButtonLabel' => $data['buy_button_cta'],
 	);
 }
 ?>
--- web/app/themes/learning-cms-theme/inc/blocks/data/core-button.php
+++ web/app/themes/learning-cms-theme/inc/blocks/data/core-button.php
@@ -9,21 +9,56 @@ function get_block_data_core_button( $block ) {
 		$innerHTML = resolve_html_children( $a );
 	}
 
-	$variant = resolve_attrs_classname_variant( $block['attrs'] );;
+	$variant = resolve_attrs_classname_variant( $block['attrs'] );
 	$icon = null;
 	if ( $variant ) {
 		if ( substr( $variant, 0, 5 ) === 'icon-' ) {
 			$icon = str_replace( 'icon-', '', $variant );
-			$variant = 'icon';
+		}
+	}
+
+	$textAlign = resolve_attrs_prop( $block['attrs'], 'align', 'left' );
+
+	$editorOptions = get_theme_support( 'editor-color-palette' )[0];
+	$white = null;
+	$black = null;
+	foreach ( $editorOptions as $option ) {
+		if ( $option['slug'] === 'white' ) {
+			$white = $option['color'];
+		}
+		if ( $option['slug'] === 'black' ) {
+			$black = $option['color'];
+		}
+	}
+	$textColor = resolve_attrs_color( $block['attrs'], 'textColor', 'customTextColor' );
+	$backgroundColor = resolve_attrs_color( $block['attrs'], 'backgroundColor', 'customBackgroundColor' );
+	$outlineColor = 'rgba(0,0,0,0)';
+
+	$class = resolve_attrs_prop( $block['attrs'], 'className' );
+	// default colors with / without outline
+	if ( strpos( $class, 'is-style-outline' ) !== false ) {
+		if (!$backgroundColor) {
+			$backgroundColor = 'rgba(0,0,0,0)';
+		}
+		if (!$textColor) {
+			$textColor = $black;
+		}
+		$outlineColor = $textColor;
+	} else {
+		if (!$backgroundColor) {
+			$backgroundColor = $black;
+		}
+		if (!$textColor) {
+			$textColor = $white;
 		}
 	}
 
 	return array(
-		'variant'         => $variant,
 		'icon'            => $icon,
-		'textAlign'       => resolve_attrs_prop( $block['attrs'], 'align', 'left' ),
-		'textColor'       => resolve_attrs_color( $block['attrs'], 'textColor', 'customTextColor' ),
-		'backgroundColor' => resolve_attrs_color( $block['attrs'], 'backgroundColor', 'customBackgroundColor' ),
+		'textAlign'       => $textAlign,
+		'textColor'       => $textColor,
+		'outlineColor'    => $outlineColor,
+		'backgroundColor' => $backgroundColor,
 		'link'            => $link,
 		'innerHTML'       => $innerHTML
 	);
